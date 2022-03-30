7001eaa93aa8a113b42b4e26a3887966c31ad623
vlfwns924 <vlfwns924@gamil.com>
2022-03-30 11:29:01 +0900

    ..

---
+++ src/main/java/kr/co/lgit/boot/SpringbootWebservicApplication.java
@@ -0,0 +1,13 @@
+package kr.co.lgit.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/lgit/boot/SpringbootWebservicApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.lgit.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebservicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
