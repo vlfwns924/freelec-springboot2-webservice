624fb44cd1f7b869485f6c6171a96714df8bcd54
vlfwns924 <vlfwns924@gmail.com>
2022-04-06 23:03:35 +0900

    ..

--- src/main/java/kr/co/lgit/boot/controller/HelloController.java
+++ src/main/java/kr/co/lgit/boot/controller/HelloController.java
@@ -8,7 +8,7 @@ import kr.co.lgit.boot.dto.HelloResponseDto;
 
 @RestController //@RestController는 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다.
 //예전에 @ResponseBody를 각 메소드마다 선언했던 것을 한번에 사용할 수 있게 해준다고 생각하면 된다.
-public class HelloController {
+public class HelloController { 
 
 	@GetMapping("/hello") //HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 준다.
 	public String Hello() {
