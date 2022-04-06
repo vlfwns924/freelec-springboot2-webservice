41d2f67a8025e26667cd927588db5dafe6032056
vlfwns924 <vlfwns924@gmail.com>
2022-04-06 09:02:12 +0900

    ..

--- src/main/java/kr/co/lgit/boot/controller/HelloController.java
+++ src/main/java/kr/co/lgit/boot/controller/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 	@GetMapping("/hello/dto")
 	public HelloResponseDto helloDto(@RequestParam("name") String name,  
 									@RequestParam("amount") int amount){
-		return new HelloResponseDto(name, amount);
+		return new HelloResponseDto(name, amount); 
 		//@RequestParam : 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션 
 	}
 }
