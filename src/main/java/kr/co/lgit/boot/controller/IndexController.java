ea26f388ab92a44677f19d40290f6d1171368558
vlfwns924 <vlfwns924@gmail.com>
2022-04-06 20:28:09 +0900

    ..

--- src/main/java/kr/co/lgit/boot/controller/IndexController.java
+++ src/main/java/kr/co/lgit/boot/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import kr.co.lgit.boot.config.auth.dto.SessionUser;
 import kr.co.lgit.boot.dto.PostsResponseDto;
 import kr.co.lgit.boot.service.PostsService;
 import lombok.RequiredArgsConstructor;
